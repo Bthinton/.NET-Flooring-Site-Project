fd0c5abdea438305e83aef6bcbd3e442bb6dc198
Brad <bthinton101@gmail.com>
2017-10-19 16:04:54 -0400

    Add project files.

---
+++ Flooringsite/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Flooringsite
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Flooringsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Flooringsite.MvcApplication" Language="C#" %>
